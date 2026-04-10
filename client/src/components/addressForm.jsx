2d39402814a565931d147b284aaddee106abb340
Jonah Barofski <jbarofski@gmail.com>
2026-04-10 13:43:02 -0400

    centered address form elements

--- client/src/components/addressForm.jsx
+++ client/src/components/addressForm.jsx
@@ -25,43 +25,6 @@ export default function AddressForm({ address, onSubmit, setAddress }) {
         streetAndNumber={address.streetAndNumber}
       />
 
-
-      <label htmlFor="city">City</label>
-      <input
-        type="text"
-        id="city"
-        placeholder="City"
-        value={address.place}
-        onChange={(event) => handleManualInputChange(event, "place")}
-      />
-
-      <label htmlFor="state">State/Province/Region</label>
-      <input
-        type="text"
-        id="state"
-        placeholder="State/Province/Region"
-        value={address.region}
-        onChange={(event) => handleManualInputChange(event, "region")}
-      />
-
-      <label htmlFor="postcode">Postcode</label>
-      <input
-        type="text"
-        id="postcode"
-        placeholder="Postcode"
-        value={address.postcode}
-        onChange={(event) => handleManualInputChange(event, "postcode")}
-      />
-
-      <label htmlFor="country">Country</label>
-      <input
-        type="text"
-        id="country"
-        placeholder="Country"
-        value={address.country}
-        onChange={(event) => handleManualInputChange(event, "country")}
-      />
-
       <div className="buttons">
         <button type="submit" className="confirm-button">
           Confirm
